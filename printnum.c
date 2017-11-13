7d3e11459a1ad163a2b91928d6327c25906df4ff
Yung-Hsiang Lu <yhluprog@gmail.com>
2017-11-12 23:22:16 -0500

    create printnum.c

---
+++ printnum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char * argv[])
+{
+  for (int count = 0; count < 10; count ++)
+  {
+    printf("%d\n", count);
+  }
+  return EXIT_SUCCESS;
+}
